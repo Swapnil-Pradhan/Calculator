68eab9e7d6b1b14ac3e027e7915b853607e4ce3c
Swapnil <90128544+Swapnil-Pradhan@users.noreply.github.com>
2022-01-13 12:07:43 +0530

    Update Update.js

--- Update.js
+++ Update.js
@@ -1,5 +1,6 @@
 var update=document.getElementById("updatebg");
 if(document.getElementById("update").innerHTML<2.01){
+document.querySelector("a").href="Calculator.apk";
 update.style.display="flex";
 update.style.opacity="1";
 document.getElementById("version").style.innerHTML+=")";
